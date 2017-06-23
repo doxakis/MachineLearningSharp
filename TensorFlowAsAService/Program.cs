de65aedad382dd26d20687323c0d69de26898913
Philip Doxakis <philip@doxakis.com>
2017-06-23 00:07:56 -0400

    proof of concept: tensorflow could be exposed as a service (based on inception_v3)

---
+++ TensorFlowAsAService/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TensorFlowAsAService
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var projectDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            var modelInceptionV3 = Path.Combine(projectDir, "inception_v3.pb");
+            var labelsFile = Path.Combine(projectDir, "imagenet_comp_graph_label_strings.txt");
+
+            var modelContent = File.ReadAllBytes(modelInceptionV3);
+            var labels = File.ReadAllLines(labelsFile);
+            var filesContent = Directory.EnumerateFiles(projectDir + @"\samples", "*", SearchOption.AllDirectories)
+                .Select(m => File.ReadAllBytes(m))
+                .ToList();
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // One line to get the prediction from tensorflow. (model: InceptionV3 only)
+            // - App must run on 64 bit.
+            // - Model and jpeg can be read from file/database/etc.
+            // - Labels is the classification labels.
+
+            List<FileClassificationResult> result = TensorFlowClassificationWrapper.FindLabel(modelContent, labels, filesContent);
+
+            stopwatch.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine("Duration: " + stopwatch.Elapsed.ToString());
+            Console.WriteLine("Files: " + filesContent.Count);
+            Console.WriteLine("Processor count: " + Environment.ProcessorCount);
+
+            Console.WriteLine("Milliseconds per picture: " + (stopwatch.ElapsedMilliseconds / filesContent.Count));
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TensorFlowAsAService/TensorFlowClassificationWrapper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace TensorFlowAsAService
+{
+    public class FileClassificationResult
+    {
+        public string File { get; internal set; }
+        public string Label { get; internal set; }
+        public float Percent { get; internal set; }
+        public float[] Bottleneck { get; internal set; }
+        public int ClassificationId { get; internal set; }
+    }
+
+    public class TensorFlowClassificationWrapper
+    {
+        public static List<FileClassificationResult> FindLabel(byte[] model, string[] labels, IEnumerable<byte[]> files)
+        {
+            List<FileClassificationResult> listOfFileClassificationResult = new List<FileClassificationResult>();
+
+            var graph = new TFGraph();
+
+            GC.Collect();
+            graph.Import(model, "");
+            
+            using (var session = new TFSession(graph))
+            {
+                foreach (var file in files)
+                {
+                    FileClassificationResult fileResult = new FileClassificationResult();
+
+                    var tensor = CreateTensorFromImageFile(file);
+                    var runner = session.GetRunner();
+
+                    TFOutput classificationLayer = graph["softmax"][0];
+                    TFOutput bottleneckLayer = graph["pool_3"][0];
+
+                    TFOutput tIn = graph["DecodeJpeg"][0];
+                    runner.AddInput(tIn, tensor).Fetch(classificationLayer, bottleneckLayer);
+                    var output = runner.Run();
+
+                    // Bottleneck result.
+                    var result = output[1];
+                    var values = ((Single[][][][])result.GetValue(jagged: true))[0][0][0];
+                    fileResult.Bottleneck = values;
+
+                    // Classification result.
+                    result = output[0];
+
+                    var bestIdx = 0;
+                    float p = 0, best = 0;
+                    var probabilities = ((float[][])result.GetValue(jagged: true))[0];
+                    for (int i = 0; i < probabilities.Length; i++)
+                    {
+                        if (probabilities[i] > best)
+                        {
+                            bestIdx = i;
+                            best = probabilities[i];
+                        }
+                    }
+                    fileResult.Label = labels[bestIdx];
+                    fileResult.ClassificationId = bestIdx;
+                    fileResult.Percent = best * 100.0f;
+
+                    listOfFileClassificationResult.Add(fileResult);
+                }
+            }
+            return listOfFileClassificationResult;
+        }
+
+        // Convert the image in filename to a Tensor suitable as input to the Inception model.
+        static TFTensor CreateTensorFromImageFile(byte[] contents)
+        {
+            // DecodeJpeg uses a scalar String-valued tensor as input.
+            var inputTensor = TFTensor.CreateString(contents);
+            TFGraph graph = new TFGraph();
+            TFOutput input = graph.Placeholder(TFDataType.String);
+            TFOutput output = graph.DecodeJpeg(contents: input, channels: 3);
+
+            using (var session = new TFSession(graph))
+            {
+                var tensor = session.Run(
+                    inputs: new[] { input },
+                    inputValues: new[] { inputTensor },
+                    outputs: new[] { output });
+                return tensor[0];
+            }
+        }
+    }
+}
