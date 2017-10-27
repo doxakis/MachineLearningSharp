4009ce3d4651dc27d77a86e3ce2a89a39c2bd6f6
Philip Doxakis <doxakis@users.noreply.github.com>
2017-10-27 14:24:59 -0400

    update

--- TensorFlowAsAService/Program.cs
+++ TensorFlowAsAService/Program.cs
@@ -11,84 +11,87 @@ namespace TensorFlowAsAService
     {
         public static void Main(string[] args)
         {
-            var projectDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
-            var modelInceptionV3 = Path.Combine(projectDir, "inception_v3.pb");
-            var labelsFile = Path.Combine(projectDir, "imagenet_comp_graph_label_strings.txt");
+			for (int i = 0; i < 10; i++)
+			{
+				var projectDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+				var modelInceptionV3 = Path.Combine(projectDir, "inception_v3.pb");
+				var labelsFile = Path.Combine(projectDir, "imagenet_comp_graph_label_strings.txt");
 
-            var modelContent = File.ReadAllBytes(modelInceptionV3);
-            var labels = File.ReadAllLines(labelsFile);
-            var srcDir = projectDir + @"\samples";
-            
-            var files = Directory.EnumerateFiles(srcDir, "*", SearchOption.AllDirectories)
-                .Select(m => m.ToLower())
-                .Where(m => m.EndsWith(".png") || m.EndsWith(".jpeg") || m.EndsWith(".jpg") || m.EndsWith(".bmp"))
-                .OrderBy(m => Path.GetFileNameWithoutExtension(m))
-                .ToList();
+				var modelContent = File.ReadAllBytes(modelInceptionV3);
+				var labels = File.ReadAllLines(labelsFile);
+				var srcDir = projectDir + @"\samples";
 
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
+				var files = Directory.EnumerateFiles(srcDir, "*", SearchOption.AllDirectories)
+					.Select(m => m.ToLower())
+					.Where(m => m.EndsWith(".png") || m.EndsWith(".jpeg") || m.EndsWith(".jpg") || m.EndsWith(".bmp"))
+					.OrderBy(m => Path.GetFileNameWithoutExtension(m))
+					.ToList();
 
-            // One line to get the prediction from tensorflow. (model: InceptionV3 only)
-            // - App must run on 64 bit.
-            // - Model and jpeg can be read from file/database/etc.
-            // - Labels is the classification labels.
+				Stopwatch stopwatch = new Stopwatch();
+				stopwatch.Start();
 
-            List<FileClassificationResult> result = TensorFlowClassificationWrapper.FindLabel(modelContent, labels, files);
+				// One line to get the prediction from tensorflow. (model: InceptionV3 only)
+				// - App must run on 64 bit.
+				// - Model and jpeg can be read from file/database/etc.
+				// - Labels is the classification labels.
 
-            // Show result.
+				List<FileClassificationResult> result = TensorFlowClassificationWrapper.FindLabel(modelContent, labels, files);
 
-            var labelForOCR = new string[]
-            {
-                "menu",
-                "web site",
-                "book jacket",
-                "envelope",
-                "packet",
-                "letter opener",
-                "scoreboard",
-                "binder"
-            };
+				// Show result.
 
-            foreach (var fileResult in result)
-            {
-                // Predictions.
-                var predictions = fileResult.Predictions.OrderByDescending(m => m.Percent)
-                    .Where(m => m.Percent >= 0.10)
-                    .Take(5);
-                
-                // Use OCR ?
-                if (predictions.Any(m => labelForOCR.Contains(m.Label)))
-                {
-                    // File.
-                    Console.WriteLine("You should run OCR on " + Path.GetFileNameWithoutExtension(fileResult.File));
+				var labelForOCR = new string[]
+				{
+					"menu",
+					"web site",
+					"book jacket",
+					"envelope",
+					"packet",
+					"letter opener",
+					"scoreboard",
+					"binder"
+				};
 
-                    //foreach (var prediction in predictions)
-                    //{
-                    //    Console.WriteLine(" = " + prediction.Label + " (" + prediction.Percent + ")");
-                    //}
-                }
-                else
-                {
-                    Console.WriteLine("Look like there is no text on " + Path.GetFileNameWithoutExtension(fileResult.File));
+				foreach (var fileResult in result)
+				{
+					// Predictions.
+					var predictions = fileResult.Predictions.OrderByDescending(m => m.Percent)
+						.Where(m => m.Percent >= 0.10)
+						.Take(5);
 
-                    //foreach (var prediction in predictions)
-                    //{
-                    //    Console.WriteLine(" = " + prediction.Label + " (" + prediction.Percent + ")");
-                    //}
-                }
-            }
+					// Use OCR ?
+					if (predictions.Any(m => labelForOCR.Contains(m.Label)))
+					{
+						// File.
+						Console.WriteLine("You should run OCR on " + Path.GetFileNameWithoutExtension(fileResult.File));
 
-            stopwatch.Stop();
+						//foreach (var prediction in predictions)
+						//{
+						//    Console.WriteLine(" = " + prediction.Label + " (" + prediction.Percent + ")");
+						//}
+					}
+					else
+					{
+						Console.WriteLine("Look like there is no text on " + Path.GetFileNameWithoutExtension(fileResult.File));
 
-            Console.WriteLine();
-            Console.WriteLine("Duration: " + stopwatch.Elapsed.ToString());
-            Console.WriteLine("Files: " + files.Count);
-            Console.WriteLine("Processor count: " + Environment.ProcessorCount);
+						//foreach (var prediction in predictions)
+						//{
+						//    Console.WriteLine(" = " + prediction.Label + " (" + prediction.Percent + ")");
+						//}
+					}
+				}
 
-            Console.WriteLine("Milliseconds per picture: " + (stopwatch.ElapsedMilliseconds / files.Count));
+				stopwatch.Stop();
 
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadLine();
+				Console.WriteLine();
+				Console.WriteLine("Duration: " + stopwatch.Elapsed.ToString());
+				Console.WriteLine("Files: " + files.Count);
+				Console.WriteLine("Processor count: " + Environment.ProcessorCount);
+
+				Console.WriteLine("Milliseconds per picture: " + (stopwatch.ElapsedMilliseconds / files.Count));
+
+				Console.WriteLine("Press any key to continue...");
+				Console.ReadLine();
+			}
         }
     }
 }
